890adaff0303e28556d5efecbf7ca565acf69614
michaszek <punkomichal@gmail.com>
2025-11-21 22:02:23 +0100

    go workflow with my own code

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) int {
+	return a / b
+}
+
 func main() {
-	println("Hello, world!")
+	println(add(1, 2))
+	println(sub(1, 2))
+	println(mul(1, 2))
+	println(div(1, 2))
 }
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := add(1, 2)
+	want := 3
+	if got != want {
+		t.Errorf("add(1, 2) = %d, want %d", got, want)
+	}
+}
+
+func TestSub(t *testing.T) {
+	got := sub(4, 3)
+	want := 1
+	if got != want {
+		t.Errorf("sub(4, 3) = %d, want %d", got, want)
+	}
+}
+
+func TestDiv(t *testing.T) {
+	got := div(9, 3)
+	want := 3
+	if got != want {
+		t.Errorf("div(9, 3) = %d, want %d", got, want)
+	}
+}
+
+func TestMul(t *testing.T) {
+	got := mul(4, 2)
+	want := 8
+	if got != want {
+		t.Errorf("mul(4, 2) = %d, want %d", got, want)
+	}
+}
